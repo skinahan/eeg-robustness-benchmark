21daaa33bbe901bb173a5e65d08dfed2c33a05c9
Sean Kinahan <kinahan.sean@gmail.com>
2025-06-20 09:44:41 -0700

    Configuration files for REEGNet and CNN-NCP for BNCI2014_001 MotorImagery Dataset.

---
+++ download_datasets.py
@@ -0,0 +1,47 @@
+# Utility Script to download MNE datasets locally.
+
+import mne
+# Motor Imagery Datasets
+from moabb.datasets import BNCI2014_001
+from moabb.datasets import BNCI2014_004
+from moabb.datasets import BNCI2015_001
+from moabb.datasets import Lee2019_MI
+from moabb.datasets import Zhou2016
+# P300
+from moabb.datasets import BNCI2014_009
+from moabb.datasets import EPFLP300
+# SSVEP
+from moabb.datasets import Lee2019_SSVEP
+
+def download_dataset(dataset):
+    print(f"Starting dataset {dataset.code} download...")
+    # Force download and cache for all subjects
+    for subject in dataset.subject_list:
+        try:
+            dataset._get_single_subject_data(subject)
+            print(f"Subject {subject} downloaded successfully.")
+        except Exception as e:
+            print(f"Error downloading subject {subject}: {e}")
+
+def download_bnci2014_001():
+    dataset = BNCI2014_001()
+    return download_dataset(dataset)
+
+def download_bnci2014_004():
+    dataset = BNCI2014_004()
+    return download_dataset(dataset)
+
+def download_bnci2014_009():
+    dataset = BNCI2014_009()
+    return download_dataset(dataset)
+
+def download_lee2019_SSVEP():
+    dataset = Lee2019_SSVEP()
+    return download_dataset(dataset)
+
+if __name__ == "__main__":
+
+    # download_bnci2014_001()
+    # download_bnci2014_004()
+    # download_bnci2014_009()
+    download_lee2019_SSVEP()
